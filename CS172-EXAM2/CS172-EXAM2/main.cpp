32ef0d6d59ac2557c6b6b7e1d404cd3f79393b1b
Megan Bruening <mbruening18@my.whitworth.edu>
2016-11-10 12:56:40 -0800

    Initial commit

---
+++ CS172-EXAM2/CS172-EXAM2/main.cpp
@@ -0,0 +1,30 @@
+//
+//  main.cpp
+//  CS172-EXAM2
+//
+//  Created by Megan Bruening on 11/10/16.
+//  Copyright © 2016 Megan Bruening. All rights reserved.
+//
+
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <string>
+#include <ctime>
+#include <cstdlib>
+#include <cctype>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    
+    
+    
+    
+    
+    
+    return 0;
+}
